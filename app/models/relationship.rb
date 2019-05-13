0b92d06c9ccfa5e5f4233e405e179e0aa785cf12
explodicus <dan.gutu.97@gmail.com>
2019-05-13 17:01:49 +0300

    S3 configuration

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -4,9 +4,9 @@ class Relationship < ApplicationRecord
   belongs_to :group
 
   def set_color
-    relationships = Relationship.where('user_id = ? AND color NOT NULL', user_id)
+    relationships = Relationship.where('user_id = ? AND color IS NOT NULL', user_id)
     used_colors = Array.new
-    relationships.each {|relationship| used_colors.append relationship.color}
+    relationships.each { |relationship| used_colors.append relationship.color }
     COLORS.each do |color|
       next if used_colors.include?(color)
       self.color = color
