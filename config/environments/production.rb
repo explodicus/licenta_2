adda91bf70efab4706b0dcbbd6cbbc894ad089b8
explodicus <dan.gutu.97@gmail.com>
2019-05-13 15:11:26 +0300

    Changed no-reply address

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,8 +69,8 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'isceo.herokuapp.com'
-  config.action_mailer.default_url_options = { host: 'isceo.herokuapp.com' }
+  host = 'dry-thicket-41508.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
       :address        => 'smtp.sendgrid.net',
       :port           => '587',
