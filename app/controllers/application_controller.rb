461b78bb4a1007d69274d027a39df14d205833ed
explodicus <dan.gutu.97@gmail.com>
2019-06-19 18:09:56 +0300

    More static pages, Pundit redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :set_locale
 
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
   protected
 
   def configure_permitted_parameters
@@ -34,5 +36,10 @@ class ApplicationController < ActionController::Base
   def extract_locale_from_accept_language_header
     parsed_locale = params[:locale] || request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/)[0]
     I18n.available_locales.map(&:to_s).include?(parsed_locale) ? parsed_locale : I18n.default_locale
+  end
+
+  def user_not_authorized
+    flash[:alert] = t("You don't seem to have access to view this page Please log in as a different user to see it")
+    redirect_to root_path
   end
 end
