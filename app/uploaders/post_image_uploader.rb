87389f19b66db34eeeb59f6dc488bc4cd7fbd892
explodicus <dan.gutu.97@gmail.com>
2019-06-26 13:07:21 +0300

    Post image restyling

--- app/uploaders/post_image_uploader.rb
+++ app/uploaders/post_image_uploader.rb
@@ -5,7 +5,7 @@ class PostImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
-  process resize_to_limit: [770, 310]
+  process resize_to_limit: [1000, 1000]
 
   # Choose what kind of storage to use for this uploader:
   if Rails.env.production?
